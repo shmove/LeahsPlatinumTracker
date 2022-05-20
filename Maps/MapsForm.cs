8b097bc80aca336f9abfcaa98c4e0887d3ec1a06
shmove <matthewliddle03@gmail.com>
2022-05-20 01:42:54 +0100

    Fixed issue with WarpButtons not displaying properly.

--- Maps/MapsForm.cs
+++ Maps/MapsForm.cs
@@ -31,6 +31,7 @@ namespace LeahsPlatinumTracker
                     pictureBox.SendToBack();
                 }
             }
+            UpdateWarpAppearances();
         }
 
         public void Reload()
