bb34991c373b242a7e8c3207115495c3c105876b
shmove <matthewliddle03@gmail.com>
2022-06-14 23:33:04 +0100

    Updated ToString method for Checks subclasses.

--- Classes/Checks.cs
+++ Classes/Checks.cs
@@ -68,6 +68,8 @@
 
         string ToString();
 
+        Dictionary<string, string> GetLocalisedFlagStrings();
+
     }
 
     /// <summary>
@@ -129,58 +131,6 @@
             return true;
         }
 
-        // String localisation
-
-        /// <summary>
-        /// A dictionary of strings and their readable stringified versions, localised to current program language.
-        /// </summary>
-        public static Dictionary<string, string> LocalisedFlagStrings
-        {
-            get
-            {
-                Dictionary<string, Dictionary<string,string>> TranslatedStrings = new Dictionary<string, Dictionary<string, string>>
-                {
-                    { "HasWorksKey",            new Dictionary < string, string > { { "en", "Obtain the Works Key" } } },
-                    { "HasGalacticKey",         new Dictionary < string, string > { { "en", "Obtain the Galactic Key" } } },
-                    { "HasTeleport",            new Dictionary < string, string > { { "en", "Obtain a Pokémon with Teleport" } } },
-                    { "HasBike",                new Dictionary < string, string > { { "en", "Obtain a Bike" } } },
-                    { "HasSpokenRoark",         new Dictionary < string, string > { { "en", "Speak to Roark in Oreburgh Mine" } } },
-                    { "HasSpokenFantina",       new Dictionary < string, string > { { "en", "Speak to Fantina in the Super Contest Hall" } } },
-                    { "HasSpokenVolkner",       new Dictionary < string, string > { { "en", "Speak to Volkner in the Vista Lighthouse" } } },
-                    { "HasDefeatedWindworks",   new Dictionary < string, string > { { "en", "Defeat Mars in Valley Windworks" } } },
-                    { "HasSecretPotion",        new Dictionary < string, string > { { "en", "Obtain the SecretPotion from Cynthia" } } },
-                    { "HasCoalBadge",           new Dictionary < string, string > { { "en", "Obtain the Coal Badge" } } },
-                    { "HasForestBadge",         new Dictionary < string, string > { { "en", "Obtain the Forest Badge" } } },
-                    { "HasRelicBadge",          new Dictionary < string, string > { { "en", "Obtain the Relic Badge" } } },
-                    { "HasCobbleBadge",         new Dictionary < string, string > { { "en", "Obtain the Cobble Badge" } } },
-                    { "HasFenBadge",            new Dictionary < string, string > { { "en", "Obtain the Fen Badge" } } },
-                    { "HasMineBadge",           new Dictionary < string, string > { { "en", "Obtain the Mine Badge" } } },
-                    { "HasIcicleBadge",         new Dictionary < string, string > { { "en", "Obtain the Icicle Badge" } } },
-                    { "HasBeaconBadge",         new Dictionary < string, string > { { "en", "Obtain the Beacon Badge" } } },
-                    { "HasAaron",               new Dictionary < string, string > { { "en", "Defeat Elite Four Aaron" } } },
-                    { "HasBertha",              new Dictionary < string, string > { { "en", "Defeat Elite Four Bertha" } } },
-                    { "HasFlint",               new Dictionary < string, string > { { "en", "Defeat Elite Four Flint" } } },
-                    { "HasLucian",              new Dictionary < string, string > { { "en", "Defeat Elite Four Lucian" } } },
-                    { "HasCynthia",             new Dictionary < string, string > { { "en", "Become Champion" } } },
-                    { "HM01",                   new Dictionary < string, string > { { "en", "Obtain HM01 (Cut)" } } },
-                    { "HM02",                   new Dictionary < string, string > { { "en", "Obtain HM02 (Fly)" } } },
-                    { "HM03",                   new Dictionary < string, string > { { "en", "Obtain HM03 (Surf)" } } },
-                    { "HM04",                   new Dictionary < string, string > { { "en", "Obtain HM04 (Strength)" } } },
-                    { "HM05",                   new Dictionary < string, string > { { "en", "Obtain HM05 (Defog)" } } },
-                    { "HM06",                   new Dictionary < string, string > { { "en", "Obtain HM06 (Rock Smash)" } } },
-                    { "HM07",                   new Dictionary < string, string > { { "en", "Obtain HM07 (Waterfall)" } } },
-                    { "HM08",                   new Dictionary < string, string > { { "en", "Obtain HM08 (Rock Climb)" } } }
-                };
-
-                Dictionary<string, string> LocalisedStrings = new();
-                foreach(KeyValuePair<string, Dictionary<string, string>> entry in TranslatedStrings)
-                {
-                    LocalisedStrings.Add(entry.Key, entry.Value[Program.Language]);
-                }
-
-                return LocalisedStrings;
-            }
-        }
     }
 
 }
